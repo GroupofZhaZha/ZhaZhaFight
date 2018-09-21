745c1cccfc9dd40df5a6481ed3c54bb0730fe91c
Graydee Chen <zehuiche@buffalo.edu>
2018-09-21 17:41:23 -0400

    I have added few panels.
    I have added the  Credit Panel, Option Panel and which we can access it by clicking the credit and option button on the main menu.

---
+++ ZhaZhaFight/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour {
+    
+	public void PlayGameButtonHandler()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void BackToMainMenuPanelButtonHandler()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void OptionButtonHandler()
+    {
+        SceneManager.LoadScene("Option");
+    }
+
+    public void CreditButtonHandler()
+    {
+        SceneManager.LoadScene("Credit");
+    }
+
+    public void QuitButtonHandler()
+    {
+        Application.Quit();
+    }
+
+
+
+}
--- ZhaZhaFight/Assets/Start.cs
+++ ZhaZhaFight/Assets/Start.cs
@@ -7,6 +7,6 @@ public class Start : MonoBehaviour {
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Game");
     }
 }
